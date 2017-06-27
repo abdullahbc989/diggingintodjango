3e5d8384c9bb233342b43b95cf728f2d7e9ba3c9
Abdullah Clute <clute.abdullahb@gmail.com>
2017-06-27 13:48:31 -0500

    Added form functionality
    Need to fix form bug

--- Django_Project/Treasuregram/main_app/urls.py
+++ Django_Project/Treasuregram/main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     # This URL looks for a number and captures that value to send to the detail view
     url(r'^([0-9]+)/$', views.detail, name="detail"),
+    url(r'^post_url/$', views.post_treasure, name='post_treasure'),
 ]
--- Django_Project/Treasuregram/main_app/views.py
+++ Django_Project/Treasuregram/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Treasure
+from django.http import HttpResponse
+from .forms import TreasureForm
 """If you want to import a specific class from your current app,
 you can leave off the package and type the following:
 from .module import class (or function)"""
@@ -7,9 +9,21 @@ from .module import class (or function)"""
 # Create your views here.
 def index(request):
     treasures = Treasure.objects.all()
-    return render(request, 'index.html', {'treasures':treasures})
+    form = TreasureForm()
+    return render(request, 'index.html', {'treasures':treasures}, {'form':form})
 
 
 def detail(request, treasure_id):
     treasure = Treasure.objects.get(id=treasure_id)
     return render(request, 'detail.html', {'treasure': treasure})
+
+def post_treasure(request):
+    form = TreasureForm(request.POST)
+    if form.is_valid():
+        treasure = Treasure(name = form.cleaned_data['name'],
+                            value = form.cleaned_data['value'],
+                            material = form.cleaned_data['material'],
+                            location = form.cleaned_data['location'],
+                            img_url = form.cleaned_data['img_url'],)
+        treasure.save()
+    return HttpResponseRedirect('/')
